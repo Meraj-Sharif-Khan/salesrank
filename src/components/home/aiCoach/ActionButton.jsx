3c9b41689468f24cffa02e5a84b7ab47060859a3
Meraj-Sharif-Khan <merajkhan.mk101@gmail.com>
2025-05-03 02:33:18 +0600

    Update - Ai coach section button cursor pointer

--- src/components/home/aiCoach/ActionButton.jsx
+++ src/components/home/aiCoach/ActionButton.jsx
@@ -9,35 +9,35 @@ const ActionButton = () => {
   return (
     <div className="inline-block">
       <div className="p-2 lg:p-6 flex flex-row lg:flex-col flex-nowrap gap-10 bg-white rounded-full shrink-0">
-        <button className="shrink-0">
+        <button className="shrink-0 cursor-pointer">
           <img
             className="h-6 w-6 object-scale-down"
             src={restoreIcon}
             alt="restore Icon"
           />
         </button>
-        <button className="shrink-0">
+        <button className="shrink-0 cursor-pointer">
           <img
             className="h-6 w-6 object-scale-down"
             src={likeIcon}
             alt="like Icon"
           />
         </button>
-        <button className="shrink-0">
+        <button className="shrink-0 cursor-pointer">
           <img
             className="h-6 w-6 object-scale-down"
             src={dislikeIcon}
             alt="dislike Icon"
           />
         </button>
-        <button className="shrink-0">
+        <button className="shrink-0 cursor-pointer">
           <img
             className="h-6 w-6 object-scale-down"
             src={pastIcon}
             alt="past Icon"
           />
         </button>
-        <button className="shrink-0">
+        <button className="shrink-0 cursor-pointer">
           <img
             className="h-6 w-6 object-scale-down"
             src={recordIcon}
